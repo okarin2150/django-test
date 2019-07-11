9ef9cdc29b3769d677ff8af5bd7323717d925804
okarin <ni.manteghi98@yahoo.com>
2019-07-11 10:55:23 -0700

    income in models and some zibasazi

--- web/models.py
+++ web/models.py
@@ -8,5 +8,14 @@ from django.contrib.auth.models import User
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
-    ammount = models.BigIntegerField()
+    amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
+class Income(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
