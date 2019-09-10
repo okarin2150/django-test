9951d8bcebcd8411a7ac20c93c57aec53fa15f10
okarin <ni.manteghi98@yahoo.com>
2019-09-10 00:18:07 -0700

    created the resgister function so users can now register and use the site

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^submit/expense/$',views.submit_expense,name='submit_expense')
-    url(r'^submit/income/$',views.submit_income,name='submit_income')
+    url(r'^submit/expense/$',views.submit_expense,name='submit_expense'),
+    url(r'^submit/income/$',views.submit_income,name='submit_income'),
+    url(r'^accounts/register/$', views.register, name='register'),
+    url(r'^$', views.index, name='index'),
+    url(r'^accounts/logout/?$', views.logout_auth, name='logout'),
+
 ]
